6018504d805fd4ab68a8a17d5cd0fa05bf738712
AverageName <max.pakhomov@mail.ru>
2018-08-24 22:58:14 +0300

    FCN's for semantic segmentation

---
+++ pytorch_image_segmentation/models/fcn_16s.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class Vgg_16_fcn16(nn.Module):
+    
+    
+    def __init__(self,num_classes = 1000):
+        super(Vgg_16_fcn16,self).__init__()
+        
+        self.model = models.vgg16(pretrained=True)
+        
+        self.num_classes = num_classes
+        
+        self.classifier = nn.Sequential(
+                nn.Conv2d(512,4096,7,padding = 3),
+                nn.ReLU(),
+                nn.Dropout(),
+                nn.Conv2d(4096,4096,1,padding = 0),
+                nn.ReLU(),
+                nn.Dropout(),
+                nn.Conv2d(4096,num_classes,1,padding = 0))
+        
+        self.conv = nn.Conv2d(4096,num_classes,1)
+        
+        self.features_skip = nn.Sequential()
+        
+        self.features_rest = nn.Sequential()
+        
+        for i in range(24):
+            self.features_skip.add_module('{}'.format(i),self.model.features[i])
+        
+        for i in range(24,31):
+            self.features_rest.add_module('{}'.format(i),self.model.features[i])
+        
+        
+        
+        if num_classes == 1000:
+            self.classifier[0].weight.data = (self.model.classifier[0].weight.data).view(4096,512,7,7)
+            self.classifier[3].weight.data = (self.model.classifier[3].weight.data).view(4096,4096,1,1)
+            self.classifier[6].weight.data = (self.model.classifier[6].weight.data).view(1000,4096,1,1)
+            self.classifier[0].bias.data = self.model.classifier[0].bias.data
+            self.classifier[3].bias.data = self.model.classifier[3].bias.data
+            self.classifier[6].bias.data = self.model.classifier[6].bias.data
+            self.conv.weight.data = torch.zeros(num_classes,512,1,1)
+            self.conv.bias.data = torch.zeros(num_classes)
+            
+        else:
+            self.classifier[0].weight.data = (self.model.classifier[0].weight.data).view(4096,512,7,7)
+            self.classifier[3].weight.data = (self.model.classifier[3].weight.data).view(4096,4096,1,1)
+            self.classifier[6].weight.data = torch.randn(num_classes,4096,1,1)
+            self.classifier[0].bias.data = self.model.classifier[0].bias.data
+            self.classifier[3].bias.data = self.model.classifier[3].bias.data
+            self.classifier[6].bias.data = torch.randn(num_classes)
+            self.conv.weight.data = torch.zeros(num_classes,512,1,1)
+            self.conv.bias.data = torch.zeros(num_classes)
+        
+    def forward(self,x):
+        size = (x.shape[2], x.shape[3])
+        x = self.features_skip(x)
+        y = x
+        y = self.conv(y)
+        x = self.features_rest(x)
+        x = self.classifier(x)
+        x = nn.functional.upsample(x, size =(y.shape[2],y.shape[3]), mode='bilinear', align_corners=True) 
+        return nn.functional.upsample((x+y), size=size, mode='bilinear', align_corners=True)
---
+++ pytorch_image_segmentation/models/fcn_32s.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class Vgg_16_fcn32(nn.Module):
+    
+    
+    def __init__(self,num_classes = 1000):
+        super(Vgg_16_fcn32,self).__init__()
+        
+        self.model = models.vgg16(pretrained=True)
+        
+        self.num_classes = num_classes
+        
+        self.classifier = nn.Sequential(
+                nn.Conv2d(512,4096,7,padding = 3),
+                nn.ReLU(),
+                nn.Dropout(),
+                nn.Conv2d(4096,4096,1,padding = 0),
+                nn.ReLU(),
+                nn.Dropout(),
+                nn.Conv2d(4096,num_classes,1,padding = 0),
+                nn.Upsample(scale_factor=32,mode='bilinear'))
+        
+        if num_classes == 1000:
+            self.classifier[0].weight.data = (self.model.classifier[0].weight.data).view(4096,512,7,7)
+            self.classifier[3].weight.data = (self.model.classifier[3].weight.data).view(4096,4096,1,1)
+            self.classifier[6].weight.data = (self.model.classifier[6].weight.data).view(1000,4096,1,1)
+            self.classifier[0].bias.data = self.model.classifier[0].bias.data
+            self.classifier[3].bias.data = self.model.classifier[3].bias.data
+            self.classifier[6].bias.data = self.model.classifier[6].bias.data
+        else:
+            self.classifier[0].weight.data = (self.model.classifier[0].weight.data).view(4096,512,7,7)
+            self.classifier[3].weight.data = (self.model.classifier[3].weight.data).view(4096,4096,1,1)
+            self.classifier[6].weight.data = torch.randn(num_classes,4096,1,1)
+            self.classifier[0].bias.data = self.model.classifier[0].bias.data
+            self.classifier[3].bias.data = self.model.classifier[3].bias.data
+            self.classifier[6].bias.data = torch.randn(num_classes)
+        
+    def forward(self,x):
+        size = (x.shape[2],x.shape[3])
+        x = self.model.features(x)
+        x = self.classifier(x)
+        return nn.functional.upsample(x, size=size,mode='bilinear', align_corners=True)
+    
